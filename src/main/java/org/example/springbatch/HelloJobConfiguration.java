57cb3287c82c311db2612f3499559814ac1bfe32
seungbin.kim <seungbin.kim.dev@gmail.com>
2025-04-02 00:02:30 +0900

    hello spring batch

---
+++ src/main/java/org/example/springbatch/HelloJobConfiguration.java
@@ -0,0 +1,49 @@
+package org.example.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job helloJob() {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return new StepBuilder("helloStep1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello Spring Batch - 1");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return new StepBuilder("helloStep2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello Spring Batch - 2");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+}
